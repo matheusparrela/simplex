a99d2de3dc77d7b09d50a3d31933a2517378336a
Éricles Miller <ericlesmiller15@gmail.com>
2022-11-14 19:28:06 -0300

    vs4

--- pesquisaOperacional/pesquisaOperacional/settings.py
+++ pesquisaOperacional/pesquisaOperacional/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'app',
     'crispy_forms',
+
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
