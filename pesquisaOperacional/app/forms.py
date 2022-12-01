462f2f33d3c895721d377579da1d0db40c6d71e3
Éricles Miller <ericlesmiller15@gmail.com>
2022-11-30 21:40:49 -0300

    DualPrimal

--- pesquisaOperacional/app/forms.py
+++ pesquisaOperacional/app/forms.py
@@ -2,13 +2,25 @@ from django import forms
 #from .models import Variable
         
 class MyForm(forms.Form):
+    # interface para escolher metodo
+    IMethod = [
+        ('PRIMAL', 'Primal'),
+        ('DUAL', 'Dual')
+    ]
+    
     numeroRestricoes       =  forms.IntegerField(label='Número de Restrições',widget=forms.NumberInput(attrs={'class':'form-control'}))
     numeroVariaveisDecisao =  forms.IntegerField(label='Número de variáveis', widget=forms.NumberInput(attrs={'class':'form-control'}))
-            
+    method = forms.ChoiceField(choices=IMethod, widget=forms.RadioSelect(attrs={'class':'form-check-input'}), label='Método')
+    
                             
 class GenerateProblemSimplex(forms.Form):
     def __init__(self, numeroVariaveisDecisao, numeroRestricoes, *args, **kwargs):
         super(GenerateProblemSimplex, self).__init__(*args, **kwargs)
+        
+        
+        objectiveFunction = [('MAXIMIZE', 'Maximizar'), ('MINIMIZE', 'Minimizar')]
+        self.fields['objective'] = forms.ChoiceField(choices= objectiveFunction, label='objective', widget=forms.Select(attrs={'class':'form-control'}))
+               
 
         for i in range(numeroVariaveisDecisao + 1):
             if i != (numeroVariaveisDecisao): # check if i > 5
