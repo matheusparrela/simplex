d7de3463f98e3651fe6e7288e877c800843c5364
Matheus M. Parrela <92748188+matheusparrela@users.noreply.github.com>
2022-12-08 21:19:41 -0300

    SensitivityAnalysis
    Classe que faz a análise de sensibilidade do problema simplex.

--- scripts/SensitivityAnalysis.py
+++ scripts/SensitivityAnalysis.py
@@ -1,2 +1,10 @@
-def analysis(problema):
-    pass
+import numpy as np
+
+
+class SensitivityAnalysis:
+
+    def __init__(self, problem):
+        self.problem = problem
+
+    def analysis(self):
+        pass
