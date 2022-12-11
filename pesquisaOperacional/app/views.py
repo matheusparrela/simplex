4b46524ce3ee2c6c86ef0b8a8ff5281af1848869
Éricles Miller <ericlesmiller15@gmail.com>
2022-12-11 20:34:06 -0300

    tabular

--- pesquisaOperacional/app/views.py
+++ pesquisaOperacional/app/views.py
@@ -53,6 +53,46 @@ def problemVariables(request):
             'sliceObjet'   : f'1:{numVariable + 1}'
         })
 
-def table(request): 
-    return redirect(request, 'table.html')
+def threePass(request):
+    if request.session['IMethod'] == "GRAPHIC":
+        return redirect('/grafica')
+    else:
+        return redirect('/tabular')
     
+    
+def tabular_view(request):
+    try:
+        json_request = make_json(request.session)
+        json_request = json.dumps(json_request)
+        json_response = simplex.main.solve_simplex(json_request)
+        json_response = json.loads(json_response)
+
+        if len(json_response["error_msg"]) > 0:
+            raise Exception("Solution Error:" + json_response["error_msg"])
+
+        headers = ["Base", "z"] + json_response["result"]["variables"] + ["b"]
+        interactions = json_response["result"]["iterations"]
+        optimum_point = json_response["result"]["optimum_point"]
+        optimum_value = json_response["result"]["optimum_value"]
+        tables = []
+
+        for iter in interactions:
+            current_table = []
+            lineZ = ["z", "1"] + iter["z"]["coeficients"]
+            lineZ.append(iter["z"]["value"])
+            current_table.append(lineZ)
+
+            for exp in iter["expressions"]:
+                current_line = [exp["base"], "0"] + exp["coeficients"]
+                current_line.append(exp["value"])
+                current_table.append(current_line)
+
+            tables.append(current_table)
+
+        return render(request, 'resultado_tabular.html',
+                      context={"tables": tables, "headers": headers, "optimum_point": optimum_point,
+                               "optimum_value": optimum_value})
+    except Exception as e:
+        print(e)
+        messages.error(request, 'Algo de inesperado aconteceu. Verifique as entradas.')
+        return redirect('/')
